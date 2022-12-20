9b5444877472999fe2e8386c381749822a9c3e5e
mockxe <38732499+mockxe@users.noreply.github.com>
2022-12-20 20:53:45 +0100

    Initial commit 🥙

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("application")
+    id("java")
+}
+
+group = "io.mockxe.uni.s1.a9.map"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "s1_a9_map"
+
